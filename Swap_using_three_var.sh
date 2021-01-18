d4a5c6b4dd5a6a0a973dd9a7d954979f26c9d276
Dhruv-RKC <58837188+Dhruv-RKC@users.noreply.github.com>
2021-01-18 13:36:29 +0530

    Create Swap_using_three_var.sh

---
+++ Swap_using_three_var.sh
@@ -0,0 +1,12 @@
+echo "Enter Numbers for Swapping"
+read firstNumber
+read secondNumber
+echo "Before Swapping"
+echo "First number: $firstNumber"
+echo "Second number: $secondNumber"
+temp=$firstNumber
+firstNumber=$secondNumber
+secondNumber=$temp
+echo "After Swapping"
+echo "First number: $firstNumber"
+echo "Second number: $secondNumber"
