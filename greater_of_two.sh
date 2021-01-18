2a9a15fc62c2809d036d693b6ab34a6a97b1ea85
Dhruv-RKC <58837188+Dhruv-RKC@users.noreply.github.com>
2021-01-18 13:57:35 +0530

    Update greater_of_two.sh

--- greater_of_two.sh
+++ greater_of_two.sh
@@ -5,7 +5,7 @@ if [ $firstnumber -gt $secondnumber ]
 then 
 echo "$firstnumber is greater than $secondnumber"
 fi
-if [ $firstnumber -lt $secondnumber ]
+if [ $secondnumber -gt $firstnumber ]
 then
 echo "$secondnumber is greater than $firstnumber"
 fi
