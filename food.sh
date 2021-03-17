49fc9dd86cb586d0157fca6813aad220659c7d46
Dhruv-RKC <58837188+Dhruv-RKC@users.noreply.github.com>
2021-03-17 10:16:25 +0530

    Create food.sh

---
+++ food.sh
@@ -0,0 +1,31 @@
+echo "-----FOOD MENU-----"
+echo "B= Burger"
+echo "F= French Fries"
+echo "P= Pizza"
+echo "S= Sandwiches"
+echo "Enter Your Choice: "
+read ch
+case $ch in
+B) echo "Enter Quantity of Burger: "
+   read q
+   echo "Total Charges: `expr $q \* 200`"
+   ;;
+F) echo "Enter Quantity of French Fries: "
+   read q
+   echo "Total Charges: `expr $q \* 50`"
+   ;;
+P) echo "Enter Quantity of Pizza: "
+   read q
+   echo "Total Charges: `expr $q \* 500`"
+   ;;
+S) echo "Enter Quantity of Sandwiches: "
+   read q
+   echo "Total Charges: `expr $q \* 150`"
+   ;;
+*)
+ echo "Sorry we don't Provide that Food Item"
+ echo "Kindly, check your choice again!"
+ ;;
+esac    
+
+
